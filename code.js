7ea2b3ee3250b103d02b2c2504e22b66d410e70b
DeVante Bailey <devantebailey1322@gmail.com>
2019-11-15 18:18:07 -0500

    updated html

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ $(function(){
 
     $(".saveBtn").click(function(){
         var userEvent = $("#input").val();
-        localStorage.setItem("getvalue,input");
+        localStorage.setItem("getvalue,userEvent");
         
     });
     
