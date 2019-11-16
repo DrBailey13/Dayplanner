26279cd3360a7e70e522193c84dfc13234ed1fb5
DeVante Bailey <devantebailey1322@gmail.com>
2019-11-16 16:03:50 -0500

    display local storage

--- code.js
+++ code.js
@@ -1,75 +1,126 @@
 
 
 
+$(".saveBtn").click(function () {
+    var userEvent = $("#input9").val();
+    localStorage.setItem("9am", userEvent);
 
-    $(".saveBtn").click(function(){
-        var userEvent = $("#input9").val();
-        localStorage.setItem("9am",userEvent);  
-        
-    });
-function DisplayEvent(){
+});
+function DisplayEvent9() {
     $("#input9").val(localStorage.getItem("9am"))
 
 
-    }
+}
 
-    DisplayEvent();
-    
-      
+DisplayEvent9();
 
-    
 
-    
-    $(".saveBtn").click(function(){
-        var userEvent = $("#input10").val();
-        localStorage.setItem("10am",userEvent);  
-        
-    });
-    
-    $(".saveBtn").click(function(){
-        var userEvent = $("#input11").val();
-        localStorage.setItem("11am",userEvent);  
-        
-    });
-    
-    $(".saveBtn").click(function(){
-        var userEvent = $("#input12").val();
-        localStorage.setItem("12pm",userEvent);  
-        
-    });
+$(".saveBtn").click(function () {
+    var userEvent = $("#input10").val();
+    localStorage.setItem("10am", userEvent);
 
-    $(".saveBtn").click(function(){
-        var userEvent = $("#input1").val();
-        localStorage.setItem("1pm",userEvent);  
-        
-    });
-    
-    $(".saveBtn").click(function(){
-        var userEvent = $("#input2").val();
-        localStorage.setItem("2pm",userEvent);  
-        
-    });
-    
-    $(".saveBtn").click(function(){
-        var userEvent = $("#input3").val();
-        localStorage.setItem("3pm",userEvent);  
-        
-    });
-    
-    $(".saveBtn").click(function(){
-        var userEvent = $("#input4").val();
-        localStorage.setItem("4pm",userEvent);  
-        
-    });
+});
+function DisplayEvent10() {
+    $("#input10").val(localStorage.getItem("10am"))
 
-    $(".saveBtn").click(function(){
-        var userEvent = $("#input5").val();
-        localStorage.setItem("5pm",userEvent);  
-        
-    });
- 
-   
-    
+
+}
+
+DisplayEvent10();
+
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#input11").val();
+    localStorage.setItem("11am", userEvent);
+
+});
+function DisplayEvent11() {
+    $("#input11").val(localStorage.getItem("11am"))
+
+
+}
+
+DisplayEvent11();
+
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#input12").val();
+    localStorage.setItem("12pm", userEvent);
+
+});
+function DisplayEvent12() {
+    $("#input12").val(localStorage.getItem("12pm"))
+
+
+}
+
+DisplayEvent12();
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#input1").val();
+    localStorage.setItem("1pm", userEvent);
+
+});
+function DisplayEvent1() {
+    $("#input1").val(localStorage.getItem("1pm"))
+
+
+}
+
+DisplayEvent1();
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#input2").val();
+    localStorage.setItem("2pm", userEvent);
+
+});
+function DisplayEvent2() {
+    $("#input2").val(localStorage.getItem("2pm"))
+
+
+}
+
+DisplayEvent2();
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#input3").val();
+    localStorage.setItem("3pm", userEvent);
+
+});
+function DisplayEvent3() {
+    $("#input3").val(localStorage.getItem("3pm"))
+
+
+}
+
+DisplayEvent3();
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#input4").val();
+    localStorage.setItem("4pm", userEvent);
+
+});
+function DisplayEvent4() {
+    $("#input4").val(localStorage.getItem("4pm"))
+
+
+}
+
+DisplayEvent4();
+
+$(".saveBtn").click(function () {
+    var userEvent = $("#input5").val();
+    localStorage.setItem("5pm", userEvent);
+
+});
+function DisplayEvent5() {
+    $("#input5").val(localStorage.getItem("5pm"))
+
+
+}
+
+DisplayEvent5();
+
 
 
 
