14c00bea5605492adeefe0ef5704d3b127ef7e15
DeVante Bailey <devantebailey1322@gmail.com>
2019-11-16 14:59:52 -0500

    display local storage

--- code.js
+++ code.js
@@ -1,11 +1,75 @@
-$(function(){
+
+
+
 
     $(".saveBtn").click(function(){
-        var userEvent = $("#input").val();
-        localStorage.setItem("getvalue",userEvent);  
+        var userEvent = $("#input9").val();
+        localStorage.setItem("9am",userEvent);  
+        
+    });
+function DisplayEvent(){
+    $("#input9").val(localStorage.getItem("9am"))
+
+
+    }
+
+    DisplayEvent();
+    
+      
+
+    
+
+    
+    $(".saveBtn").click(function(){
+        var userEvent = $("#input10").val();
+        localStorage.setItem("10am",userEvent);  
         
     });
     
-});
+    $(".saveBtn").click(function(){
+        var userEvent = $("#input11").val();
+        localStorage.setItem("11am",userEvent);  
+        
+    });
+    
+    $(".saveBtn").click(function(){
+        var userEvent = $("#input12").val();
+        localStorage.setItem("12pm",userEvent);  
+        
+    });
+
+    $(".saveBtn").click(function(){
+        var userEvent = $("#input1").val();
+        localStorage.setItem("1pm",userEvent);  
+        
+    });
+    
+    $(".saveBtn").click(function(){
+        var userEvent = $("#input2").val();
+        localStorage.setItem("2pm",userEvent);  
+        
+    });
+    
+    $(".saveBtn").click(function(){
+        var userEvent = $("#input3").val();
+        localStorage.setItem("3pm",userEvent);  
+        
+    });
+    
+    $(".saveBtn").click(function(){
+        var userEvent = $("#input4").val();
+        localStorage.setItem("4pm",userEvent);  
+        
+    });
+
+    $(".saveBtn").click(function(){
+        var userEvent = $("#input5").val();
+        localStorage.setItem("5pm",userEvent);  
+        
+    });
+ 
+   
+    
+
 
 
